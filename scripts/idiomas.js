6486e7fd999083dbf1440d6026d5a5b512bc6c60
Alex <alexander.galvez@estudioenpenascal.com>
2023-10-04 10:48:39 +0200

    arreglos script idiomas

--- scripts/idiomas.js
+++ scripts/idiomas.js
@@ -1,5 +1,3 @@
-import {encabezadoNav1, encabezadoNav2, encabezadoNav3, encabezadoTitle, encabezadoText, encabezadoBtn } from "..scripts/objetosDeIdiomas.js";
-
 let idiomas=document.querySelectorAll(".nav-link");
 let tituloHeader=document.querySelector(".Cont-header__presentIturritek-title");
 let textoHeader=document.querySelector(".Cont-header__presentIturritek-text");
@@ -7,12 +5,39 @@ let vinculosHeader=document.querySelectorAll(".linkscript");
 let btnContact=document.querySelector(".contactanosbtn");
 
 
+const encabezadoNav1= {
+    en:"ABOUT US",
+    eu:"",
+}
+const encabezadoNav2={
+    en:"SERVICES",
+    eu:"",
+}
+const encabezadoNav3={
+    en:"PEÑASCAL GROUP",
+    eu:"",
+}
+const encabezadoTitle={
+    en:"2O YEARS OF EXPERIENCE",
+    eu:"",
+}
+const encabezadoText={
+    en:"Iturritek is an entrepreneurial reality that punishes projects and installations, maintenance and"
+    +"mountings as a reference and that contributes to the generation of wealth and employment in our society, from "
+    +"considerations, values and social styles.",
+    eu:"",
+}
+const encabezadoBtn={
+    en:"Contact Us",
+    eu:"", 
+}
+
 idiomas.forEach((idioma)=>{
     idioma.addEventListener("click",()=>{
         if(idioma.classList.contains("en")){
             idiomaEn()
-            recorrernav()
-            console.log(encabezadoNav1.en)
+            recorrernavEn()
+          
         }
         else if(idioma.classList.contains("eu")){
             
@@ -24,7 +49,7 @@ idiomas.forEach((idioma)=>{
    
 })
 
-function recorrernav(){
+function recorrernavEn(){
     for (let i=0;i<vinculosHeader.length;i++){
         vinculosHeader[0].textContent=encabezadoNav1.en
         vinculosHeader[1].textContent=encabezadoNav2.en

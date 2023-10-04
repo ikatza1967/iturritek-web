03c62a8995304496a8dccd85b4e786d40819a7e4
Alex <alexander.galvez@estudioenpenascal.com>
2023-10-04 10:18:07 +0200

    idioma ingles js problema modulos import

--- scripts/idiomas.js
+++ scripts/idiomas.js
@@ -1,8 +1,40 @@
+import {encabezadoNav1, encabezadoNav2, encabezadoNav3, encabezadoTitle, encabezadoText, encabezadoBtn } from "..scripts/objetosDeIdiomas.js";
+
 let idiomas=document.querySelectorAll(".nav-link");
+let tituloHeader=document.querySelector(".Cont-header__presentIturritek-title");
+let textoHeader=document.querySelector(".Cont-header__presentIturritek-text");
+let vinculosHeader=document.querySelectorAll(".linkscript");
+let btnContact=document.querySelector(".contactanosbtn");
+
 
 idiomas.forEach((idioma)=>{
     idioma.addEventListener("click",()=>{
-        idioma.classList.contains("en");
-        alert("hola")
+        if(idioma.classList.contains("en")){
+            idiomaEn()
+            recorrernav()
+            console.log(encabezadoNav1.en)
+        }
+        else if(idioma.classList.contains("eu")){
+            
+        }
+        else if(idioma.classList.contains("es")){
+            location.reload()
+        }
     })
+   
 })
+
+function recorrernav(){
+    for (let i=0;i<vinculosHeader.length;i++){
+        vinculosHeader[0].textContent=encabezadoNav1.en
+        vinculosHeader[1].textContent=encabezadoNav2.en
+        vinculosHeader[2].textContent=encabezadoNav3.en
+     }
+}
+
+function idiomaEn(){
+tituloHeader.textContent=encabezadoTitle.en;
+textoHeader.textContent=encabezadoText.en;
+btnContact.textContent=encabezadoBtn.en;
+}
+
---
+++ scripts/objetosDeIdiomas.js
@@ -0,0 +1,34 @@
+
+let encabezadoNav1= {
+    en:"ABOUT US",
+    eu:"",
+}
+const encabezadoNav2={
+    en:"SERVICES",
+    eu:"",
+}
+const encabezadoNav3={
+    en:"PEÑASCAL GROUP",
+    eu:"",
+}
+const encabezadoTitle={
+    en:"2O YEARS OF EXPERIENCE",
+    eu:"",
+}
+const encabezadoText={
+    en:"Iturritek is an entrepreneurial reality that punishes projects and installations, maintenance and"
+    +"mountings as a reference and that contributes to the generation of wealth and employment in our society, from "
+    +"considerations, values and social styles.",
+    eu:"",
+}
+const encabezadoBtn={
+    en:"Contact Us",
+    eu:"", 
+}
+export default{
+    encabezadoNav1,
+    encabezadoNav2,
+    encabezadoNav3,
+    encabezadoTitle,
+    encabezadoText,
+    encabezadoBtn,}

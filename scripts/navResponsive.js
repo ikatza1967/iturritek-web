a347b915f1d9c258b535267de72900ee2f80479c
Alex <alexander.galvez@estudioenpenascal.com>
2023-10-05 08:31:22 +0200

    burguer programing deleted

--- scripts/navResponsive.js
+++ scripts/navResponsive.js
@@ -1,22 +1,22 @@
-let barra = document.querySelector('.cont-apartados')
-let btnNav = document.querySelector('.btn-nav')
+// let barra = document.querySelector('.cont-apartados')
+// let btnNav = document.querySelector('.btn-nav')
 let logoFter= document.querySelector('.logo-footer')
 
-btnNav.addEventListener('click', () => {
-  if (barra.style.display === 'block') {
-    barra.style.display = 'none'
-  } 
-  else if(barra.style.display === 'none') {
-    barra.style.display = 'block'
-  }
-})
+// btnNav.addEventListener('click', () => {
+//   if (barra.style.display === 'block') {
+//     barra.style.display = 'none'
+//   } 
+//   else if(barra.style.display === 'none') {
+//     barra.style.display = 'block'
+//   }
+// })
 
-document.addEventListener('click', (e) => {
-  if (e.target.classList.contains('linkscript') || e.target.classList.contains('es')
-   || e.target.classList.contains('eu') || e.target.classList.contains('en')) {
-    barra.style.display = 'none'
-  }
-})
+// document.addEventListener('click', (e) => {
+//   if (e.target.classList.contains('linkscript') || e.target.classList.contains('es')
+//    || e.target.classList.contains('eu') || e.target.classList.contains('en')) {
+//     barra.style.display = 'none'
+//   }
+// })
     
     logoFter.addEventListener('click', () => {
       location.href = 'index.html'

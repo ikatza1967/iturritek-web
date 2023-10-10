add2ed546af7bc543b06ea57c5a28495445b8a87
SpikeSoap <alazgorjs@gmail.com>
2023-10-10 16:03:00 +0200

    subida traducción legal

---
+++ scripts/idiomasPolitica.js
@@ -0,0 +1,197 @@
+
+const politicTitleA = {
+    title: ["Legal warning", "Legeen abisua"],
+    desc: [
+        "This legal notice (hereinafter, the “Legal Notice”) regulates the use of the Internet portal service www.iturritek.com (hereinafter, the “Website”) of Iturritek SL with CIF B95325445 with registered office Plaza Belategi 1 Bj 48002 - (Bilbao) - Vizcaya.",
+        "Lege-ohar honek (aurrerantzean, “Lege Oharra”) Iturritek SLren www.iturritek.com Interneteko atariaren zerbitzuaren erabilera (aurrerantzean, “Webgunea”) arautzen du B95325445 IFK duen egoitza Plaza Belategi 1 Bj 48002 - (Bilbo). ) - Bizkaia.",
+    ],
+    sub1:[
+        "Legislation",
+        "Legeria",
+    ],
+    text1:[
+        "In general, the relations between Peñascal Kooperatiba with the Users of its telematic services, present on the website, are subject to Spanish legislation and jurisdiction. The parties expressly renounce any jurisdiction that may apply to them and expressly submit to the Courts and Tribunals of Vizcaya to resolve any controversy that may arise in the interpretation or execution of these contractual conditions.",
+        "Oro har, Peñascal Kooperatibak bere zerbitzu telematikoen Erabiltzaileekin dituen harremanak, webgunean daudenak, Espainiako legediaren eta jurisdikzioaren mende daude. Alderdiek berariaz uko egiten diote aplika dakiekeen eskumenari, eta espresuki Bizkaiko Epaitegi eta Auzitegien esku jartzen dira kontratu-baldintza hauek interpretatzean edo betetzean sor daitekeen edozein eztabaida ebazteko."
+    ],
+    sub2:[
+        "Content and use",
+        "Edukia eta erabilera"
+    ],
+    text2:[
+        "The User is informed, and accepts, that access to this website does not imply, in any way, the beginning of a commercial relationship with Peñascal Kooperatiba. The owner of the website does not identify itself with the opinions expressed therein by its collaborators. Peñascal Kooperatiba reserves the right to make without prior notice any modifications it deems appropriate to its Website, and may change, delete or add the contents and services that are included. provided through it as the way in which they appear presented or located on their servers.",
+        "Erabiltzaileari jakinarazten zaio, eta onartzen du, webgune honetara sartzeak ez duela esan nahi, inola ere, Peñascal Kooperatibarekin harreman komertzial bat hastea. Webgunearen jabea ez da bere kolaboratzaileek bertan adierazitako iritziekin identifikatzen, Peñascal Kooperatibak bere webgunean egokitzat jotzen dituen aldaketak aldez aurretik jakinarazi gabe egiteko eskubidea du, eta dauden edukiak eta zerbitzuak aldatu, ezabatu edo gehi ditzake. barne, bere zerbitzarietan aurkeztuta edo kokatuta agertzeko modu gisa emana."
+    ], 
+    sub3:[
+        "Intellectual and industrial property",
+        "Jabetza intelektuala eta industriala",
+    ],
+    text3:[
+        "The intellectual property rights of the content of the web pages, their graphic design and codes are owned by Peñascal Kooperatiba and, therefore, their reproduction, distribution, public communication, transformation or any other activity that may be carried out with the contents of the website is prohibited. its web pages or even citing the sources, except with the written consent of Apodaka studio. All trade names, brands or distinctive signs of any kind contained on the Company's websites are the property of their owners and are protected by law.",
+        "Web orrietako edukien jabetza intelektualaren eskubideak, diseinu grafikoa eta kodeak Peñascal Kooperatibarena dira eta, beraz, horien erreprodukzio, banaketa, komunikazio publiko, eraldaketa edo webguneko edukiekin egin daitekeen beste edozein jarduera. debekatuta dago bere web orriak edo iturriak aipatzea, Apodaka estudioaren idatzizko baimena izan ezik. Konpainiaren webguneetan jasotako merkataritza-izen, marka edo zeinu bereizgarri guztiak haien jabeen jabetzakoak dira eta legeak babestuta daude."
+    ],
+    sub4:[
+        "Links",
+        "Estekak"
+    ],
+    text4:[
+        "The presence of links on the Peñascal Kooperatiba web pages is for informational purposes only and in no case does it imply a suggestion, invitation or recommendation regarding them.",
+        "Peñascal Kooperatiba web orrietan estekak egoteak informazio-helburua baino ez du eta inola ere ez du esan nahi horien inguruko iradokizun, gonbidapen edo gomendiorik."
+    ]
+
+
+}
+
+const politicTitleP = {
+    title: [
+        "Policies and privacy",
+        "Politikak eta pribatutasuna"
+    ],
+    desc1:[
+        "Our Privacy Policy is designed to help you understand how we collect and use the personal information you provide to us and to help you make informed decisions when using our website. When you visit our website, you may provide us with two types of information: personal information that you deliberately choose to disclose that is collected on an individual basis, and website usage information collected on an aggregate basis as you and others browse our website."
+    ],
+    desc2:[
+        "The Privacy Policy of Iturritek SL, hereinafter “the company”, is governed by compliance with current legislation on the protection of personal data: REGULATION (EU) 2016/679 OF THE EUROPEAN PARLIAMENT AND OF THE COUNCIL of 27 April 2016 (hereinafter, RGPD), and by Law 34/2002, of July 11, on Information Society Services and Electronic Commerce (LSSI-CE), as well as by Organic Law 15/1999, of December 13, on Protection of Personal Data and its implementing regulations while it remains in force. At the company we are committed to ensuring the privacy of our users' personal data. This policy, in accordance with the principle of active responsibility, will be applicable in the event that users decide to fill out a form on their website where personal data is collected, without prejudice to what is indicated in the “Privacy Clause” applicable to each form. concrete."
+    ],
+    sub1:[
+        "WHO IS RESPONSIBLE FOR THE PROCESSING OF THE DATA?"
+    ],
+    text1:[
+        "Company Name: Iturritek SL",
+        "CIF B95325445",
+        "Registered office: Plaza Belategi 1 Bj 48002 - (Bilbao) - Vizcaya.",
+        "Contact Data Protection Officer (DPO): XXX@XXX",
+    ],
+    sub2:[
+        "WHAT IS THE DATA CONSERVATION PERIOD?"
+    ],
+    text2:[
+        "In compliance with the principle of limitation of the conservation period, the data collected by the company is processed only for the time necessary and for the specific purposes for which it was collected. For all of the above and in generic terms, the company will keep your data for the duration of the contractual relationship (in the case of subscription or participation in a contest or raffle) and thereafter, only for the purpose of complying with legal obligations and responsibilities derived from such treatments depending on the applicable regulations. Once these limitation periods have expired, your data will be deleted. On the other hand, regarding the processing of your data for promotional purposes, or for sending information or newsletters, we will retain the data until we receive opposition to the contrary."
+    ],
+    sub3:[
+        "WHAT INFORMATION DO WE COLLECT?",
+    ],
+    text4:[
+        "We collect information from you when you register on our site, place an order, subscribe to our newsletter, respond to a survey or fill out a form.", 
+        "When placing an order or registering on our site, as applicable, you may be asked to enter your: name or email address. However, you can visit our site anonymously."
+    ], 
+    sub5:[
+        "ORIGIN OF DATA",
+
+    ],
+    text5:[
+        "The data processed in the company has been provided by the interested party himself.",
+    ],
+    sub6:[
+        "WHAT DO WE USE YOUR INFORMATION?",
+    ],
+    text6:[
+        "Cualquier información que recopilamos podrá usarse de una de las siguientes maneras:",
+
+    ],
+    subtitle1:[
+        "To manage subscriptions and purchases",
+    ],
+
+    subtext1:[
+        "Your information helps us manage the subscriptions and purchases you make through the company website.",
+    ],
+    subtitle2:[
+        "To personalize your experience",
+    ],
+    subtext2:[
+        "Your information helps us better respond to your individual needs.",
+
+    ],
+    subtitle3:[
+        "To improve our website",
+    ],
+    subtext3:[
+        "Nos esforzamos continuamente por mejorar nuestras ofertas de sitios web en función de la información y los comentarios que recibimos de usted."
+    ],
+    subtitle4:[
+    "Para mejorar el servicio al cliente",
+    ],
+    subtext4: [
+        "Su información nos ayuda a responder de manera más efectiva a sus solicitudes de servicio al cliente y necesidades de soporte."
+    ],
+    subtitle5:[
+        "To process transactions"
+    ],
+
+
+
+}
+
+
+
+// Su información, ya sea pública o privada, no será vendida, intercambiada, transferida o entregada a ninguna otra compañía por ningún motivo, sin su consentimiento, que no sea con el propósito expreso de entregar el producto o servicio solicitado.
+
+// Para administrar un concurso, promoción, encuesta u otra característica del sitio
+// Para enviar correos electrónicos periódicos
+// La dirección de correo electrónico que proporciona para el procesamiento de pedidos se puede usar para enviarle información y actualizaciones relacionadas con su pedido, además de recibir noticias de la empresa ocasionalmente, actualizaciones, información relacionada con productos o servicios, etc.
+
+// Nota:
+// Si en algún momento desea darse de baja de recibir correos electrónicos futuros, incluimos instrucciones detalladas para cancelar la suscripción en la parte inferior de cada correo electrónico.
+
+// ¿CÓMO PROTEGEMOS SU INFORMACIÓN?
+// La empresa se compromete a la adopción de las medidas de índole técnica y organizativa necesarias que garanticen la seguridad de los datos de carácter personal y eviten su alteración, pérdida, tratamiento o acceso no autorizado, habida cuenta del estado de la tecnología, de acuerdo con lo establecido por el RGPD y por la aún vigente LOPD y su RLOPD. Aún cuando la empresa no pueda garantizar la absoluta inexpugnabilidad de la red Internet y por tanto la violación de los datos mediante accesos fraudulentos a ellos por parte de terceros, si se compromete a realizar los análisis de riesgos exigidos por la ley y las Evaluaciones de impacto que en su caso sean necesarias.
+
+// No almacenamos ningún detalle de tarjeta de crédito o débito en ninguno de nuestros sistemas.
+// ¿DIVULGAMOS INFORMACIÓN A TERCEROS?
+// Para operar efectivamente como una empresa, es posible que tengamos que pasar su información a terceros seleccionados y de confianza. Los terceros con los que podemos compartir información son:
+
+// Google (para enviar nuestro boletín informativo por correo electrónico)
+// Facebook (para publicidad dirigida)
+// Campaña activa (para el uso del boletín electrónico)
+// Además, cualquier tercero a quien necesitemos divulgar información personal para cumplir con nuestras obligaciones legales, reglamentarias y legales. Esto puede ser como parte de un proceso legal (por ejemplo, en respuesta a una orden judicial o en respuesta a una solicitud de una agencia de aplicación de la ley, o cuando lo consideren necesario). Esto podría ser para investigar, prevenir o tomar medidas con respecto a actividades ilegales, sospecha de fraude o situaciones que impliquen amenazas potenciales a la seguridad física, la propiedad o los derechos de cualquier persona. También podría incluir el incumplimiento de las reglas de nuestro sitio web, o de otro modo permitido o de un tercero en el que haya otorgado su consentimiento.
+
+// Tenga la seguridad de que cuando subcontratamos cualquier proceso, nos aseguramos de que cualquier proveedor o contratista tenga medidas de seguridad adecuadas. También les exigiremos que cumplan con los principios de privacidad como parte de nuestro contrato con ellos.
+
+// ENLACES DE TERCEROS
+// Ocasionalmente, a nuestro criterio, podemos incluir u ofrecer productos o servicios de terceros en nuestro sitio web. Estos sitios de terceros tienen políticas de privacidad separadas e independientes. Por lo tanto, no tenemos ninguna responsabilidad por el contenido y las actividades de estos sitios vinculados. No obstante, buscamos proteger la integridad de nuestro sitio y agradecemos cualquier comentario sobre estos sitios.
+
+// MENORES DE EDAD
+// En nuestro sitio web no permitiremos intencionalmente que ninguna persona menor de 16 años nos proporcione ninguna información de identificación personal; si tienes menos de 16 años, no proporciones información sobre ti en este sitio web. En el caso de que sepamos que hemos recopilado información personal de un niño menor de 16 años, eliminaremos esa información de nuestra base de datos lo más rápido posible.
+
+// SU CONSENTIMIENTO
+// Al usar nuestro sitio, usted acepta la política de privacidad de nuestros sitios web.
+
+// CAMBIOS A NUESTRA POLÍTICA DE PRIVACIDAD
+// Si decidimos cambiar nuestra política de privacidad, publicaremos esos cambios en esta página y / o actualizaremos la fecha de modificación de la Política de privacidad a continuación.
+
+// Esta política fue revisada / modificada en diciembre de 2020.
+
+
+
+
+idiomas.forEach((idioma) => {
+    idioma.addEventListener("click", () => {
+        if (idioma.classList.contains("en")) {
+            espanol.style.backgroundColor = "transparent";
+            euskera.style.backgroundColor = "transparent";
+            idioma.style.backgroundColor = "rgb(131, 190, 224)"
+            headerEn()
+            aboutEn()
+            compSocialEn()
+            serviceEn()
+            penascalGroupTitle.textContent = encabezadoNav.link3.en
+            fterEn()
+        }
+        else if (idioma.classList.contains("eu")) {
+            espanol.style.backgroundColor = "transparent";
+            ingles.style.backgroundColor = "transparent";
+            idioma.style.backgroundColor = "rgb(131, 190, 224)"
+            headerEu();
+            aboutEu();
+            compSocialEu();
+            serviceEu();
+            penascalGroupTitle.textContent = encabezadoNav.link3.eu;
+            fterEu();
+
+        }
+        else if (idioma.classList.contains("es")) {
+            location.reload()
+        }
+    })
+
+})

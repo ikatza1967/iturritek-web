7cabf093abe87d287b2622bcb87e90f31c77f914
Trontzal <trontzal@gmail.com>
2023-10-18 14:27:47 +0200

    lista de serivcios en la intranet

--- scripts/backend/intranetServicios.js
+++ scripts/backend/intranetServicios.js
@@ -26,3 +26,43 @@ function agregarServicio(event){
         console.error("Error al hacer la solicitud: " + error);
     });
 }
+
+
+function cargarServicios() {
+    axios.get("http://127.0.0.1:4000/ver_servicios")
+        .then(response => {
+            const servicios = response.data;
+            const serviciosContainer = document.getElementById("serviciosContainer");
+
+            servicios.forEach(servicioData => {
+                if (Array.isArray(servicioData) && servicioData.length === 5) {
+                    const id = servicioData[0];
+                    const nombre = servicioData[1];
+                    const descripcion = servicioData[2];
+                    const img = servicioData[3];
+                    const categoriaId = servicioData[4];
+
+                    const li = document.createElement("li");
+                    li.innerHTML = `
+                        <span>Nombre: ${nombre}</span><br>
+                        <span>Descripción: ${descripcion}</span><br>
+                        <span>Imagen: ${img}</span><br>
+                        <span>Categoría ID: ${categoriaId}</span><br>
+                        <button class="btnEliminar" data-id="${id}">Eliminar</button><br><br>
+                    `;
+
+                    li.querySelector(".btnEliminar").addEventListener("click", () => {
+                        eliminarServicio(id);
+                    });
+
+                    serviciosContainer.appendChild(li);
+                }
+            });
+        })
+        .catch(error => {
+            console.error("Error al cargar los servicios: " + error);
+        });
+}
+
+// Llama a la función para cargar y mostrar los servicios
+cargarServicios();

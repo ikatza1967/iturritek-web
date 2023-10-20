ffba476c4ab5119919e8a84455fcac1ecb885add
Trontzal <trontzal@gmail.com>
2023-10-20 15:06:05 +0200

    codigo ordenado

--- scripts/backend/login.js
+++ scripts/backend/login.js
@@ -16,19 +16,15 @@ document.getElementById('login-form').addEventListener('submit', function(event)
         const data = response.data;
         console.log(data);
         if (data.access_token) {
-            // Inicio de sesión exitoso, puedes redirigir al usuario o mostrar un mensaje de éxito
             document.getElementById('message').textContent = 'Inicio de sesión exitoso';
             localStorage.setItem('access_token', data.access_token);
             window.location.href = '/pages/intranet.html';
         } else {
-            // Credenciales inválidas, muestra un mensaje de error
             document.getElementById('message').textContent = 'Credenciales inválidas';
         }
     })
     .catch(error => {
-        // Maneja errores de red u otros errores
         if (error.response && error.response.status === 401) {
-            // Error de autenticación (credenciales inválidas)
             document.getElementById('message').textContent = 'Credenciales inválidas';
         } else {
             document.getElementById('message').textContent = 'Ocurrió un error al iniciar sesión';
--- scripts/backend/token.js
+++ scripts/backend/token.js
@@ -3,18 +3,16 @@ const token = localStorage.getItem('access_token');
 
 if (!token) {
     // El usuario no tiene un token válido, redirigir a la página de inicio de sesión
-    window.location.href = '/pages/login.html'; // O la URL de tu página de inicio de sesión
+    window.location.href = '/pages/login.html'; 
 } else {
     // El usuario tiene un token válido, puedes cargar la página de intranet
-    // Por ejemplo, utilizando una solicitud fetch para obtener el contenido de intranet.html
     fetch('/pages/intranet.html')
-        .then(response => response.text())
-        .then(html => {
-            // Insertar el contenido de intranet.html en un contenedor en tu página principal
-            const intranetContainer = document.getElementById('intranet-container');
-            intranetContainer.innerHTML = html;
-        })
-        .catch(error => {
-            console.error('Error al cargar la página de intranet', error);
-        });
+    .then(response => response.text())
+    .then(html => {
+        const intranetContainer = document.getElementById('intranet-container');
+        intranetContainer.innerHTML = html;
+    })
+    .catch(error => {
+        console.error('Error al cargar la página de intranet', error);
+    });
 }
